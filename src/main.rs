2ad73a921c13518230c8a3117ccc6be400d1480e
kotobukid <kotobuki@daqua.jp>
2022-12-27 17:03:57 +0900

    askamaでテンプレーティング

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
-use axum::{routing::get, Router};
+use askama::Template;
+use axum::{
+    // extract,
+    http::StatusCode,
+    response::{Html, IntoResponse, Response},
+    routing::get,
+    Router,
+};
 use std::net::SocketAddr;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+    where
+        T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err)
+            ).into_response()
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/hello", get(raw_string))
+        .route("/", get(index_page));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     axum::Server::bind(&addr)
@@ -13,6 +45,11 @@ async fn main() {
 }
 
 
-async fn root() -> &'static str {
-    "hello world"
+async fn raw_string() -> &'static str {
+    "hello world(raw string)"
+}
+
+async fn index_page() -> impl IntoResponse {
+    let template = HelloTemplate { name: "taro" };
+    HtmlTemplate(template)
 }
