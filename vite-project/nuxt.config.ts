97b91e2bad4ce88065b8815f579e9a8be10c332f
kotobukid <kotobuki@daqua.jp>
2025-04-27 23:34:14 +0900

    npm update

--- vite-project/nuxt.config.ts
+++ vite-project/nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // @ts-ignore
-    devServer: {
-        host: "localhost",
-        https: false,
-        port: 3001,
-        url: "http://localhost:3001"
-    },
+  // @ts-ignore
+  devServer: {
+      host: "localhost",
+      https: false,
+      port: 3001,
+      url: "http://localhost:3001"
+  },
+
+  compatibilityDate: "2025-04-27",
 })
